2c89f8b986f45047702f1f96020067327bfd0c6a
p998bxk <arminas.snarskis@swedbank.lt>
2026-05-07 14:34:16 +0300

    Add Exercise2 - Receipt

--- Challenge_1/src/com/bank/app/Greeting.java
+++ Challenge_1/src/com/bank/app/Greeting.java
@@ -1,8 +1,7 @@
 package com.bank.app;
-
+//Exercise1
 public class Greeting {
     public static void main(String[] args) {
-//Exercise1
         System.out.println("Hello, Swedbank!");
         System.out.print("Arminas");
         System.out.println("Snarskis");
@@ -12,25 +11,5 @@ public class Greeting {
         System.out.printf("Hello, " + firstName + "!");
         System.out.println();
         System.out.printf("JDK: " + System.getProperty("java.version"));
-
-//Exercise2
-        String item1 = "Coffee";   double price1 = 2.50;
-        String item2 = "Sandwich"; double price2 = 5.90;
-        String item3 = "Water";    double price3 = 1.20;
-        double total = price1 + price2 + price3;
-        System.out.println();
-        System.out.println();
-        System.out.printf("%-14s %6s", "Item", "Price");
-        System.out.println();
-        System.out.printf("%-14s %6.2f%n", item1, price1);
-        System.out.printf("%-14s %6.2f%n", item2, price2);
-        System.out.printf("%-14s %6.2f%n", item3, price3);
-        System.out.println("----------------------");
-        System.out.println("Total: " + total);
-        System.out.println("VAT (20%)");
-        double totalInclVat = total * 1.20;
-        System.out.println("Total incl. VAT: " + totalInclVat);
-
-
     }
 }
---
+++ Challenge_1/src/com/bank/app/Receipt.java
@@ -0,0 +1,22 @@
+package com.bank.app;
+//Exercise2
+public class Receipt {
+    public static void main(String[] args) {
+        String item1 = "Coffee";   double price1 = 2.50;
+        String item2 = "Sandwich"; double price2 = 5.90;
+        String item3 = "Water";    double price3 = 1.20;
+        double total = price1 + price2 + price3;
+        System.out.println();
+        System.out.println();
+        System.out.printf("%-14s %6s", "Item", "Price");
+        System.out.println();
+        System.out.printf("%-14s %6.2f%n", item1, price1);
+        System.out.printf("%-14s %6.2f%n", item2, price2);
+        System.out.printf("%-14s %6.2f%n", item3, price3);
+        System.out.println("----------------------");
+        System.out.println("Total: " + total);
+        System.out.println("VAT (20%)");
+        double totalInclVat = total * 1.20;
+        System.out.println("Total incl. VAT: " + totalInclVat);
+    }
+}
