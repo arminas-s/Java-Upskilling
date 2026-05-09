73436bc0dade7850e229a2f144f7d6c00aff6034
p998bxk <arminas.snarskis@swedbank.lt>
2026-05-09 13:58:16 +0300

    Add Exercise 2 — Formatted Receipt

--- Challenge_1/src/com/bank/app/FormatedReceipt.java
+++ Challenge_1/src/com/bank/app/FormatedReceipt.java
@@ -1,12 +1,11 @@
 package com.bank.app;
 //Exercise 2
-public class Receipt {
+public class FormatedReceipt {
     static void main(String[] args) {
         String item1 = "Coffee";   double price1 = 2.50;
         String item2 = "Sandwich"; double price2 = 5.90;
         String item3 = "Water";    double price3 = 1.20;
         double total = price1 + price2 + price3;
-        System.out.println();
         System.out.println();
         System.out.printf("%-14s %6s", "Item", "Price");
         System.out.println();
