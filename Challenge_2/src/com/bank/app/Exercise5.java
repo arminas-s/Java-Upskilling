cd9f59918cad36528e977c6a7dfe52391cc1b7d6
p998bxk <arminas.snarskis@swedbank.lt>
2026-05-17 15:11:27 +0300

    Add Exercise5 - String & Comparison Basics

---
+++ Challenge_2/src/com/bank/app/Exercise5.java
@@ -0,0 +1,34 @@
+package com.bank.app;
+//String & Comparison Basics
+public class Exercise5 {
+    static void main() {
+        String currency = "EUR";
+        String iban = "EE382200221020145685";
+
+        System.out.printf("%-12s %7d%n", "Currency length = ", currency.length());
+        System.out.printf("%-12s %7d%n", "IBAN length     = ", iban.length());
+
+        System.out.println("Balance: " + 100 + 50);
+        System.out.println("Balance: " + (100 + 50));
+
+        String a = "EUR";
+        String b = "EUR";;
+        System.out.println("a == b: " + (a == b));
+
+        String c = new String("EUR");
+        System.out.println("a.equals(c): " + a.equals(c));
+        System.out.println("a == c: " + (a == c));
+
+        System.out.println("ACC-" + 1001);
+        System.out.println("ACC-" + 500 + 501);
+        System.out.println("ACC-" + (500 + 501));
+
+        double balance = 1_250.75;
+
+        String string1 = String.valueOf(balance);
+        System.out.println("String1: " + string1);
+
+        String string2 = "" + balance;
+        System.out.println("String2: " + string2);
+    }
+}
